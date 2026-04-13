6f834d80b0818449f3371a56c3156e1bc5df3fc2
FirasCh3 <firaschebil61@gmail.com>
2026-04-13 22:47:34 +0100

    project setup

---
+++ micro-backend/src/test/java/com/example/microbackend/MicroBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
